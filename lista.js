cec7fe5dc6ff955384e76b7c786b5004918907a7
evertonesilva <everton.evaristo31@gmail.com>
2023-07-23 11:28:26 -0300

    aulas de Js

---
+++ lista.js
@@ -0,0 +1,16 @@
+const names = ["Felipe", "João", "Julia", 10, false];
+
+const joao = names[1];
+
+console.log(names[2]);
+console.log(joao);
+
+names.push("Pedro")
+
+console.log(names)
+
+//names.unshift(20)
+
+console.log(names)
+console.log(names.indexOf("João"))
+
---
+++ strings.js
@@ -0,0 +1,14 @@
+//Strings
+
+const firstName = "Felipe";
+const lastName = "Rocha";
+const grande = "ROCHA";
+
+const names = "Felipe";
+
+console.log("Meu nome é " + firstName + " " + lastName );
+console.log(`Meu nome é ${firstName.toUpperCase()} ${grande.toLowerCase()}`);
+
+console.log(names.split(""));
+
+
