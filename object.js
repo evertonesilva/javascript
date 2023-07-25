bdfa9b953504491133f8282882fabc0ec0a5fcdc
evertonesilva <everton.evaristo31@gmail.com>
2023-07-25 09:18:56 -0300

    object

---
+++ object.js
@@ -0,0 +1,14 @@
+const person = {
+    fristName: "Felipe",
+    lastName: "Rocha",
+    age: "21",
+    hobbies: ["Assistir filmes", "Jogar bola", "Ler"]
+};
+
+const fristName = person.fristName;
+const lastName = person.lastName;
+
+console.log(fristName);
+console.log(lastName);
+console.log(person.hobbies);
+console.log(person.hobbies[0])
