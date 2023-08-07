b957fbc29381ae93f0d606d8cf82487035aece7c
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-08-06 23:58:29 -0300

    java script

---
+++ aula11/ex001.js
@@ -0,0 +1,7 @@
+console.log('O Console funcionou corretamente!');
+
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) { console.log('Você ultrapassou a velocidade permitida! MULTADO') }
+console.log('Dirigija sempre usando cinto de segurança!')
