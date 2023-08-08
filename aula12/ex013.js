6c629547013d9db13bb8e43338efd37dde2a402a
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-08-08 18:16:57 -0300

    exercicos de JS

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -1,1 +1,28 @@
 var agora = new Date()
+var diaSem = agora.getDay()
+
+console.log(diaSem)
+
+switch(diaSem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1: 
+        console.log('Segunda-Feira')
+        break
+    case 2:
+        console.log('Terça-Feira')
+        break
+    case 3:
+        console.log('Quarta-feira')
+        break
+    case 4:
+        console.log('Quinta-feira')
+        break
+    case 5:
+        console.log('Sexta-feira')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+}
