7fd8728fe3ab4e2092a4241da9e8828461d8daf7
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-08-09 16:11:01 -0300

    exercicios de js

--- exercicio13/ex015/script.js
+++ exercicio13/ex015/script.js
@@ -6,6 +6,41 @@ function verificar(){
     if (fano.value.length == 0 || fano.value > ano ){
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        window.alert('Tudo ok')
-    }
+        var fsex = document.getElementsByName('sex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                img.setAttribute('src', 'foto.xxx') // em todos
+            }//criança
+            else if (idade < 21){
+            }//Jovem
+            else if (idade < 50){
+
+            }
+            //adulto
+            else {
+                //idoso
+            } 
+        } else if (fsex[1].checked){
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10){
+
+            }//criança
+            else if (idade < 21){
+            }//Jovem
+            else if (idade < 50){
+
+            }
+            //adulto
+            else {
+                //idoso
+            } 
+        } res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    } 
+
 }
