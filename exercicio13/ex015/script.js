9a88ee45fd8a8357b29ae234c20fee835094a258
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-08-08 22:24:47 -0300

    segundo ex JS

---
+++ exercicio13/ex015/script.js
@@ -0,0 +1,6 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('data')
+    var res = window.document.getElementById('res')
+}
