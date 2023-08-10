003747f28199a71d97f1a7c57de1344737c07fec
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-08-10 09:20:14 -0300

    Atualizei

---
+++ exercicio14/ex017/script.js
@@ -0,0 +1,11 @@
+function calcular(){
+    let numero = window.document.getElementById('num')
+    let res = window.document.querySelector('select#txttabuada')
+    if (numero.value.length == 0 ){
+        window.alert('Por favor, digite um número!')
+    } else { 
+        let n = Number(numero.value)
+        
+    }
+
+}
