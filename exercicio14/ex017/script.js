0ee14734baf2e40493641dcd693c956bbf470637
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-08-10 13:45:04 -0300

    atualizei

--- exercicio14/ex017/script.js
+++ exercicio14/ex017/script.js
@@ -5,6 +5,15 @@ function calcular(){
         window.alert('Por favor, digite um número!')
     } else { 
         let n = Number(numero.value)
+        let c = 1
+        res.innerHTML = ''
+        while (c <= 10){
+            let item = window.document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `res${c}`
+            res.appendChild(item)
+            c++
+        }
         
     }
 
