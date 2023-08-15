3ba5c176a904434be3845bc42f7f6209653c1b77
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-08-14 23:39:06 -0300

    exercicios de JS

---
+++ udemy/fundamentos/nullUnderfined.js
@@ -0,0 +1,8 @@
+let valor // não iniciado
+console.log(valor)
+
+valor = null // ausência de valor
+console.log(valor)
+
+const produto = {}
+console.log(produto.preco)
---
+++ udemy/fundamentos/objecto.js
@@ -0,0 +1,21 @@
+const prod1 = {}
+
+prod1.nome = 'Leandro'
+prod1.produto = 'Celular'
+prod1.preco = 999.99
+
+console.log(prod1)
+console.log(prod1.nome)
+
+const prod2 = {
+    nome:'Camisa Polo',
+    preco: 79.90,
+    obj: {
+        blabla: 1,
+        obj:{
+            blabla: 2,
+        }
+    }
+}
+
+console.log(prod2)
