d01d41179f96b96f4ed114a6ef444a3256367484
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-08-16 11:43:44 -0300

    exercicios de JS

---
+++ udemy/fundamentos/hoisting.js
@@ -0,0 +1,13 @@
+console.log('a =', a)
+var a = 2
+console.log('a =', a)
+
+function teste (){
+    console.log('b =', b)
+    var b = 3
+    console.log('b =', b)
+}
+
+teste()
+
+console.log('b =', b)
