fab0762721b755a93e6de6a3e2c9680d1284ddf0
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-08-23 08:54:55 -0300

    exercícios de JS

--- udemy/funcao/callback1.js
+++ udemy/funcao/callback1.js
@@ -1,5 +1,11 @@
 const fabricantes = ["Mercedes", "Audi", "BMW"]
 
 function imprimir(nome, indice){
-    
+    console.log(`${indice}. ${nome}`)
 }
+fabricantes.forEach(imprimir)
+fabricantes.forEach(function(fabricantes){
+    console.log(fabricantes)
+})
+
+fabricantes.forEach(fabricantes => console.log(fabricantes)) // função arrow
