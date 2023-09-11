190bd90af3713915d5bb0b0b65d66d588fd2ffcf
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-09-11 11:27:40 -0300

    exercicios JS

---
+++ udemy/desafioFuncao/desafio10.js
@@ -0,0 +1,10 @@
+function verificarNumero (num){
+    if(num % 3 == 0){
+        return true
+    } else{
+        return false
+    }
+} 
+
+console.log(verificarNumero(6))
+console.log(verificarNumero(7))
