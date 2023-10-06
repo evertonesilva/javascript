b8892cdf8bec335139260f718fbe9e67b25fc302
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-10-06 17:42:44 -0300

    esnext

--- udemy/esnext/promise_5.js
+++ udemy/esnext/promise_5.js
@@ -1,12 +1,18 @@
 function funcionarOuNao ( valor, chanceErro){
     return new Promise((resolve, reject)=> {
-        if(Math.random() < chanceErro){
-            reject('Ocorreu um erro!')
-        } else{
-            resolve(valor)
+        try{
+            if(Math.random() < chanceErro){
+                reject('Ocorreu um erro!')
+            } else{
+                resolve(valor)
+            }
+        } catch(e){
+            reject(e)
         }
     })
 }
 
-funcionarOuNao('Testando...', 0.9)
-    .then(console.log)
+funcionarOuNao('Testando...', 0.5)
+    .then(v => console.log(`Valor: ${v}`))
+    .catch(err => console.log(`Erro: ${err}`)) // geralmente o catch fica no final
+    .then(()=> console.log('Fim!'))
