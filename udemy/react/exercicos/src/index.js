355031b0c42b2a04e51319eec0b73c2ea6e43e44
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-10-10 18:11:13 -0300

    react

--- udemy/react/exercicos/src/index.js
+++ udemy/react/exercicos/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const elemento = <h1>React 2</h1>
- ReactDOM.render(elemento, document.getElementById('root'))
+import numero1 from './componentes/numero1'
+
+ReactDOM.render(<numero1 />, document.getElementById('root'))
