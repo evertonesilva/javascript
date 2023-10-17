b6d7c1dab7d26c30e95aedcd1bc83e4d0d8b24f2
Everton Evaristo da Silva <138706406+evertonesilva@users.noreply.github.com>
2023-10-16 22:06:32 -0300

    React

--- udemy/react/exercicos/src/componentes/saudacao.jsx
+++ udemy/react/exercicos/src/componentes/saudacao.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
 export default class Saudacao extends Component {
+    
     state = {
         tipo: "Fala",
         nome: "Pedro"
+    }
+    constructor (props){
+        super(props)
+        this.setTipo = this.setTipo.bind(this)
+  
     }
     setTipo(e) {
         this.setState({ tipo: e.target.value })
@@ -18,7 +24,7 @@ export default class Saudacao extends Component {
                 <h1>{tipo} {nome}!</h1>
                 <hr />
                 <input type="text" placeholder="Tipo..." 
-                value={tipo} onChange={e => this.setTipo(e)} />
+                value={tipo} onChange={this.setTipo} />
                 <input type="text" placeholder="Nome..." value={nome} onChange={e => this.setNome(e)} />
             </div>
         )
